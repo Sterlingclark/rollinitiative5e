99b57e62865f3074cf2fc103ec689be255c80fe5
Sterling Clark <sterlingdclark13@gmail.com>
2022-02-15 11:40:22 -0500

    Changed connection to live server

--- private/database-connection.php
+++ private/database-connection.php
@@ -1,18 +1,18 @@
 <?php
 // Local server info:
 
-define("DB_SERVER", "localhost");
-define("DB_USER", "webuser");
-define("DB_PASS", "Web250153456!");
-define("DB_NAME", "rollinitiative5e");
+// define("DB_SERVER", "localhost");
+// define("DB_USER", "webuser");
+// define("DB_PASS", "Web250153456!");
+// define("DB_NAME", "rollinitiative5e");
 
 // Live server info:
 // This will be the info you have to use to login as well. 
 
-// define("DB_SERVER", "localhost");
-// define("DB_USER", "uh5sgai2pnkjp");
-// define("DB_PASS", "Micro94!");
-// define("DB_NAME", "dbvvmrwkicvyts");
+define("DB_SERVER", "localhost");
+define("DB_USER", "uh5sgai2pnkjp");
+define("DB_PASS", "Micro94!");
+define("DB_NAME", "dbvvmrwkicvyts");
 
 
 ?>
